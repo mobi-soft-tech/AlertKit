fde47c98413787ea99f73cb2a93ed93f06f117dd
shahenAnt <55664114+shahenAnt@users.noreply.github.com>
2024-02-08 13:03:58 +0400

    Generated MSAlert framework. (#1)
    * Generated MSAlert framework.
    
    * Generated new framework version of AlertKit.
    
    * Updated README file.
    
    * Updated README file.
    
    * Regenerated alert framework.
    
    * Updated README file.
    
    * README commit.
    
    * README.
    
    * Generated new version.

---
+++ MSAlert.xcframework/ios-arm64_x86_64-simulator/MSAlert.framework/Headers/MSAlert.h
@@ -0,0 +1,19 @@
+//
+//  MSAlert.h
+//  MSAlert
+//
+//  Created by Shahen on 04.05.23.
+//  Copyright © 2023 MobiSoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MSAlert.
+FOUNDATION_EXPORT double MSAlertVersionNumber;
+
+//! Project version string for MSAlert.
+FOUNDATION_EXPORT const unsigned char MSAlertVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MSAlert/PublicHeader.h>
+
+
